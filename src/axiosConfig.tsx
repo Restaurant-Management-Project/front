dd4ba7d0e899ac5e80e2236f0a5e17f162a0a5ba
Lesenco Maria <leshenko.mri@gmail.com>
2024-02-12 21:40:01 +0200

    Almost done

--- src/axiosConfig.tsx
+++ src/axiosConfig.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "https://2b1d-81-180-74-153.ngrok-free.app/api/v1/",
+  baseURL: "https://37cc-81-180-74-152.ngrok-free.app/api/v1/",
   headers: {
     "ngrok-skip-browser-warning": "skip-browser-warning",
   },
